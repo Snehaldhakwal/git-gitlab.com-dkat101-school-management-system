bf7428f6faefeceaf024986f9f7a9e35efaf2b11
dk <dkatungi@gmail.com>
2019-07-17 22:52:10 +0300

    removed deprecated class WebMvcConfigurerAdapter from SwaggerConfig

--- src/main/java/com/dk/config/SwaggerConfig.java
+++ src/main/java/com/dk/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.dk.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
@@ -20,7 +19,7 @@ import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurerAdapter {
+public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
